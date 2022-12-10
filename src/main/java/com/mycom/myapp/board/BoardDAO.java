e11cf035310d8dcfef6d4e2b4c27c8716a8c196c
s21900772 <103234587+choihjin@users.noreply.github.com>
2022-12-10 23:45:23 +0900

    Update BoardDAO.java

--- src/main/java/com/mycom/myapp/board/BoardDAO.java
+++ src/main/java/com/mycom/myapp/board/BoardDAO.java
@@ -1,4 +1,4 @@
-package com.mycom.myapp.board;
+package com.example.board;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
@@ -14,10 +14,6 @@ import java.util.List;
 
 @Repository
 public class BoardDAO {
-    Connection conn = null;
-    PreparedStatement stmt = null; //이건 무슨 역할을 하는 친구지?
-    ResultSet rs = null;
-
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
@@ -26,38 +22,20 @@ public class BoardDAO {
     }
 
     private final String BOARD_INSERT = "insert into restaurant (writer, category, restName, foodRank, star, location, callNumber, opinion) values (?,?,?,?,?,?,?,?)";
-    private final String BOARD_UPDATE = "update restaurant set category=?, restName=?, foodRank=?, star=?, location=?, callNumber=?, opinion=? where id=?";
+    private final String BOARD_UPDATE = "update restaurant set writer=?, category=?, restName=?, foodRank=?, star=?, location=?, callNumber=?, opinion=? where id=?";
     private final String BOARD_DELETE = "delete from restaurant  where id=?";
     private final String BOARD_GET = "select * from restaurant  where id=?";
     private final String BOARD_LIST = "select * from restaurant order by id desc";
 
     public int insertBoard(BoardVO vo) {
-        return jdbcTemplate.update(BOARD_INSERT, new Object[]{vo.getCategory(), vo.getWriter(),vo.getRestName(),vo.getFoodRank(), vo.getStar(), vo.getLocation(), vo.getCallNumber(), vo.getOpinion()});
+        return jdbcTemplate.update(BOARD_INSERT, new Object[]{vo.getWriter(), vo.getCategory(), vo.getRestName(),vo.getFoodRank(), vo.getStar(), vo.getLocation(), vo.getCallNumber(), vo.getOpinion()});
     }
     public int deleteBoard(int id){
         return jdbcTemplate.update(BOARD_DELETE, new Object[]{id});
     }
     public int updateBoard(BoardVO vo) {
-        return jdbcTemplate.update(BOARD_UPDATE, new Object[]{vo.getCategory(), vo.getRestName(), vo.getFoodRank(), vo.getStar(), vo.getLocation(), vo.getCallNumber(), vo.getOpinion()});
+        return jdbcTemplate.update(BOARD_UPDATE, new Object[]{vo.getWriter(), vo.getCategory(), vo.getRestName(), vo.getFoodRank(), vo.getStar(), vo.getLocation(), vo.getCallNumber(), vo.getOpinion(), vo.getId()});
 
-    }
-
-    public class BoardRowMapper implements RowMapper<BoardVO> {
-
-        @Override
-        public BoardVO mapRow(ResultSet rs, int rowNum) throws SQLException {
-            BoardVO vo = new BoardVO();
-            vo.setId(rs.getInt("id"));
-            vo.setWriter(rs.getString("writer"));
-            vo.setCategory(rs.getString("category"));
-            vo.setRestName(rs.getString("restName"));
-            vo.setFoodRank(rs.getString("foodRank"));
-            vo.setStar(rs.getInt("star"));
-            vo.setLocation(rs.getString("location"));
-            vo.setCallNumber(rs.getString("callNumber"));
-            vo.setOpinion(rs.getString("opinion"));
-            return vo;
-        }
     }
     public BoardVO getBoard(int seq){
         return jdbcTemplate.queryForObject(BOARD_GET,new Object[]{seq}, new BeanPropertyRowMapper<BoardVO>(BoardVO.class));
